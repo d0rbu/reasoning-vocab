3a3560819c875c2360c2a5ff81804065e38388da
d0rbu <hacperu2010@gmail.com>
2025-11-22 17:55:44 -0600

    adjust accelerate flags

--- slurm/train_baseline_grace.sh
+++ slurm/train_baseline_grace.sh
@@ -39,6 +39,6 @@ source .venv/bin/activate
 # Run baseline training (no reasoning vocabulary)
 # Note: Hydra configs are in exp/conf/
 # Override parameters with: key=value (e.g., training.learning_rate=1e-5)
-uv run accelerate launch --num_processes 2 \
+uv run accelerate launch \
     exp/grpo_train.py \
     model=baguettotron
